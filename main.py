80ba51427017412551f6bf17cd14735ea315b866
Warlock2006 <tron890.varlakov@yande>
2024-02-15 17:27:54 +0500

    задача Список профессий

--- main.py
+++ main.py
@@ -14,5 +14,10 @@ def training(prof):
     return render_template('training.html', prof=prof)
 
 
+@app.route('/list_prof/<list>')
+def list_prof(list):
+    return render_template('list_prof.html', list=list)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
